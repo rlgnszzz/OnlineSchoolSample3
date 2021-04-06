727ed3352f83fe967373674cfe48f18e169798d5
gihun song <gihun.song@gmail.com>
2021-04-06 16:27:21 +0900

    Upload 기능 생성

---
+++ OnlineSchoolSample3/Controllers/UploadController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace FileUpload.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload  
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "업로드에 성공";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "업로드에 실패하였습니다. 다시 시도해주세요.";
+                return View();
+            }
+        }
+    }
+}
