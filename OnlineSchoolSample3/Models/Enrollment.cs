db2d630e8eb5843f4d0cd2c9a1583bf4ef623ed1
gihun song <gihun.song@gmail.com>
2021-04-06 13:57:15 +0900

    모델 클래스들(학생, 수강, 코스) 추가

---
+++ OnlineSchoolSample3/Models/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineSchoolSample3.Models
+{
+    public enum Score
+    {
+        A,B,C,D,F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public Score? Score { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
---
+++ OnlineSchoolSample3/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineSchoolSample3.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
